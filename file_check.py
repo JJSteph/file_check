daf8660b4a8ff0add66e64ac6a95995d393d45aa
JJSteph <John.J.Stephen@gmail.com>
2022-11-28 17:33:49 -0600

    Fixed formatting

--- file_check.py
+++ file_check.py
@@ -93,12 +93,12 @@ def main():
 
     ## Give report on each path
     print('')
-    print(f'Path 1:')
+    print('Path 1:')
     print(path_1)
     print(f'{len(same_items)} / {len(path_1_contents)} files in path 1 were found in path 2.')
     print('')
 
-    print(f'Path 2:')
+    print('Path 2:')
     print(path_2)
     print(f'{len(same_items)} / {len(path_2_contents)} files in path 2 were found in path 1.')
     print('')
@@ -109,18 +109,13 @@ def main():
         print('No shared files exist between the two paths.')
 
     else:
-        print(f'Shared files (files in both paths):')
+        print('Shared files (files in both paths):')
         print(f'There were {len(same_items)} shared items found')
-        # print('')
-        # print('{:<20} {:<20} {:<30} {:<30}'.format(' ','File:', 'Path 1:', 'Path 2:'))
-
+        
         for i in range(len(same_items)):
             file_num = "File " + str(i + 1) + " / " + str(len(same_items)) + ":"
             print('')
             print(f'{file_num} {same_items[i]}')
-            # path_1_file = '/' + path_1 + '/' + same_items[i]
-            # path_2_file = '/' + path_2 + '/' + same_items[i]
-            # print(f'{file_num:<20} {same_items[i]:<20} {path_1_file:<30} {path_2_file:<30}')
 
     print('')
 
